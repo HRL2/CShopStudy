d438ee91198b93be9d1fdb14f945f679ef6ed362
HRL <ihyerim02@gmail.com>
2020-09-29 16:12:02 +0900

    WCF TEST

---
+++ HelloWorldSample/HelloWorldClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+// WCF 인터페이스를 공유하기 위해 참조 추가
+using HelloWorldHost;
+// Description : 서비스의 종점을 WCF에 알리기 위해 필요한 클래스
+using System.ServiceModel.Description;
+
+namespace HelloWorldClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri uri = new Uri("http://localhost/wcf/helloworldservice");
+
+            // 종점 생성
+            ServiceEndpoint endpoint = new ServiceEndpoint(ContractDescription.GetContract(typeof(IHelloWorld)), new BasicHttpBinding(), new EndpointAddress(uri));
+
+            // 채널 생성
+            ChannelFactory<IHelloWorld> factory = new ChannelFactory<IHelloWorld>(endpoint);
+            IHelloWorld proxy = factory.CreateChannel();
+
+            // 서비스의 메소드 호출
+            string result = proxy.SayHello();
+            (proxy as IDisposable).Dispose();
+
+            // 결과 출력
+            System.Console.WriteLine(result);
+            System.Console.ReadKey(true);
+        }
+    }
+}
---
+++ HelloWorldSample/HelloWorldHost/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace HelloWorldHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // host 생성, address 지정
+            ServiceHost host = new ServiceHost(typeof(HelloWorldWCFService), new Uri("http://localhost/wcf/helloworldService"));
+
+            // 종점 설정
+            host.AddServiceEndpoint(typeof(IHelloWorld), new BasicHttpBinding(), "");
+
+            // 호스트 open
+            host.Open();
+
+            // Console 에 키 입력되면 서비스 종료
+            System.Console.WriteLine("press any key to stop service");
+            System.Console.ReadKey(true);
+            host.Close();
+        }
+    }
+
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        [OperationContract]
+        string SayHello();
+    }
+    
+    public class HelloWorldWCFService : IHelloWorld
+    {
+        public string SayHello()
+        {
+            return "Hello WCF World";
+        }
+    }
+}
