6ec2ce847e169bc3120c528ef257879554f185e5
HRL <ihyerim02@gmail.com>
2020-10-04 22:32:48 +0900

    [WCF] svcutil.exe 를 이용한 Proxy 자동 생성 및 서비스 직접 참조 제거

---
+++ HelloWorldSample/HelloWorldProxyClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldProxyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldClient client = new HelloWorldClient("BasicHttpBinding_IHelloWorld");
+            string result = client.SayHello();
+            System.Console.WriteLine(result);
+            System.Console.ReadLine();
+        }
+    }
+}
