8235402acb87a4d3870d2a762b6b5ec38c6465af
HRL <ihyerim02@gmail.com>
2020-10-05 00:08:30 +0900

    [WCF] IIS 호스팅 테스트 - HelloWorldSite, HelloWorldSerivce 프로젝트 추가

---
+++ HelloWorldSample/HelloWorldService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        [OperationContract]
+        string SayHello();
+    }
+
+    public class HelloWorldWCFService : IHelloWorld
+    {
+        public string SayHello()
+        {
+            return "Test Hello WCF World";
+        }
+    }
+}
