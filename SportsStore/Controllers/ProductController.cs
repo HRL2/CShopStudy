4b7441fd73ee891fcaa3457900ce9d3cc15a44c5
HRL <ihyerim02@gmail.com>
2021-04-13 22:52:28 +0900

    [ASP.NET] SportsStore - 프로젝트 생성 및 Product List 출력

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        // 의존성 주입 ; 인터페이스와 연결된 개체의 인스턴스 전달
+        public ProductController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
---
+++ SportsStore/Global.asax.cs
@@ -0,0 +1,50 @@
+using Ninject;
+using Ninject.Web.Common.WebHost;
+using SportsStore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace SportsStore
+{
+    public class MvcApplication : NinjectHttpApplication
+    {
+        //protected void Application_Start()
+        //{
+        //    AreaRegistration.RegisterAllAreas();
+        //    FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        //    RouteConfig.RegisterRoutes(RouteTable.Routes);
+        //    BundleConfig.RegisterBundles(BundleTable.Bundles);
+        //}
+
+        protected override void OnApplicationStarted()
+        {
+            base.OnApplicationStarted();
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private void RegisterServices(IKernel kernel)
+        {
+            DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
+    }
+}
