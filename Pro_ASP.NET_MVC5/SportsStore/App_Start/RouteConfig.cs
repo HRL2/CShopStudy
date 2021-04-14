c25777f2fe5988f82270b00c79f81a585d9f995f
HRL <ihyerim02@gmail.com>
2021-04-14 11:23:45 +0900

    [ASP.NET] 레이아웃에 bootstrap 적용, 좌측 네비게이션 생성

--- Pro_ASP.NET_MVC5/SportsStore/App_Start/RouteConfig.cs
+++ Pro_ASP.NET_MVC5/SportsStore/App_Start/RouteConfig.cs
@@ -13,11 +13,30 @@ namespace SportsStore
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // URL : / , 모든 카테고리의 상품들에 대한 첫번째 페이지를 보여줌
+            routes.MapRoute(null, "", new { Controller = "Product", action = "List", category = (string)null, page = 1});
+
+            // URL : /Page2 , 모든 카테고리의 상품에 대한 2번째 페이지를 보여줌
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                null,
+                "Page{page}",
+                new { Controller = "Product", action = "List", category = (string)null },
+                new { page = @"\d+" });
+
+            // URL : /Soccer , Soccer 카테고리의 상품에 대한 첫번째 페이지를 보여줌
+            routes.MapRoute(
+                null,
+                "{category}",
+                new { Controller = "Product", action = "List", page = 1 });
+
+            // URL : /Soccer/Page2 , Soccer 카테고리의 상품에 대한 2번째 페이지를 보여줌
+            routes.MapRoute(
+                null,
+                "{category}/Page{page}",
+                new { Controller = "Product", action = "List" },
+                new { page = @"\d+" });
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
--- Pro_ASP.NET_MVC5/SportsStore/Controllers/ProductController.cs
+++ Pro_ASP.NET_MVC5/SportsStore/Controllers/ProductController.cs
@@ -19,17 +19,24 @@ namespace SportsStore.Controllers
             this.repository = repository;
         }
 
-        public ViewResult List(int page = 1)
+        public ViewResult List(string category, int page = 1)
         {
             ProductsListViewModel model = new ProductsListViewModel
             {
-                Products = repository.Products.OrderBy(p => p.ProductID).Skip((page - 1) * PageSize).Take(PageSize),
-                PagingInfo = new PagingInfo
+                Products = repository.Products
+                            .Where(p => category == null || p.Category == category)
+                            .OrderBy(p => p.ProductID)
+                            .Skip((page - 1) * PageSize)
+                            .Take(PageSize)
+
+                , PagingInfo = new PagingInfo
                 {
                     CurrentPage = page,
                     ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
+                    TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(p => p.Category == category).Count()
                 }
+
+                , CurrentCategory = category
             };
 
             return View(model);
