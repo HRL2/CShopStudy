8ebe2049b543563d8723e4e29434aa8d823f83d5
HRL <ihyerim02@gmail.com>
2021-04-14 18:46:06 +0900

    [ASP.NET] SportsStore - 주문 제출 >관리자에게 전자 메일을 보내는 방식으로 주문 처리하기

--- Pro_ASP.NET_MVC5/SportsStore/Controllers/CartController.cs
+++ Pro_ASP.NET_MVC5/SportsStore/Controllers/CartController.cs
@@ -12,10 +12,12 @@ namespace SportsStore.Controllers
     public class CartController : Controller
     {
         private IProductRepository repository;
+        private IOrderProcessor orderProcessor;
 
-        public CartController(IProductRepository repository)
+        public CartController(IProductRepository repository, IOrderProcessor orderProcessor)
         {
             this.repository = repository;
+            this.orderProcessor = orderProcessor;
         }
 
         public ViewResult Index(Cart cart, string returnUrl)
@@ -52,6 +54,35 @@ namespace SportsStore.Controllers
         public PartialViewResult Summary(Cart cart)
         {
             return PartialView(cart);
+        }
+
+        public ViewResult Checkout()
+        {
+            return View(new ShippingDetails());
+        }
+
+        [HttpPost]
+        public ViewResult Checkout(Cart cart, ShippingDetails shippingDetails)
+        {
+            if(cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            // ModelState.IsValid ;
+            // 데이터 주석(data annotation) 을 통해서 유효성 검사 제약조건들을 검사하고,
+            // 위반된 내용이 있다면 이를 ModelState 속성을 통해서 액션 메서드에 전달해준다.
+            // 따라서, ModelState.IsValid 속성을 확인해보면 데이터의 유효성에 문제가 있는지 여부를 확인할 수 있다.
+            if(ModelState.IsValid)
+            {
+                orderProcessor.ProcessOrder(cart, shippingDetails);
+                cart.Clear();
+                return View("Completed");
+            }
+            else
+            {
+                return View(shippingDetails);
+            }
         }
     }
 }
--- Pro_ASP.NET_MVC5/SportsStore/Infrastructure/NinjectDependencyResolver.cs
+++ Pro_ASP.NET_MVC5/SportsStore/Infrastructure/NinjectDependencyResolver.cs
@@ -3,6 +3,7 @@ using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Concreate;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,6 +26,12 @@ namespace SportsStore.Infrastructure
 
             // Ninject 에 IProductRepository 인터페이스에 대한 요청이 전달되면, EFProductRepository 클래스의 인스턴스를 생성해서 제공하도록 지시한다.
             kernel.Bind<IProductRepository>().To<EFProductRepository>();
+
+            EmailSettings emailSettings = new EmailSettings
+            {
+                WriteAsFile = bool.Parse(ConfigurationManager.AppSettings["Email.WriteAsFile"] ?? "false")
+            };
+            kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("settings", emailSettings);
         }
 
         public object GetService(Type serviceType)
